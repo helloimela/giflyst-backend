ea512739532678e7974ae9c2b0b556c650422eeb
Yudhanira Ela <ela.yudhanira@scania.com>
2020-12-09 21:34:20 +0100

    Add package information

--- authorization_code/app.js
+++ authorization_code/app.js
@@ -146,5 +146,6 @@ app.get('/refresh_token', function(req, res) {
   });
 });
 
-console.log('Listening on 8888');
-app.listen(8888);
+const port = process.env.PORT || 8888;
+console.log(`Listening on port ${port}`);
+app.listen(port);
